5eb5d68313aae31b95cf77123146cdfc95a0f537
Joshua Dinh <josh@joshuadinh.tech>
2025-02-06 16:44:14 +0900

    feature: include CampaignComments in block init

--- src/Campaigns/Blocks/blocks.ts
+++ src/Campaigns/Blocks/blocks.ts
@@ -1,13 +1,10 @@
 import * as campaignTitleBlock from './CampaignTitleBlock';
 import * as campaignCover from './CampaignCover';
 import * as campaignDonateButton from './DonateButton';
+import * as campaignComments from './CampaignComments/resources';
 
 const getAllBlocks = () => {
-    return [
-        campaignTitleBlock,
-        campaignDonateButton,
-        campaignCover
-    ];
+    return [campaignTitleBlock, campaignDonateButton, campaignCover, campaignComments];
 };
 
 getAllBlocks().forEach((block) => {
